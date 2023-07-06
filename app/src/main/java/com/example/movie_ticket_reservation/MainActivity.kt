68a9e0a07008686c07534cbabc40764de70f7921
Haidy <haidyabugom3a@gmail.com>
2023-07-06 16:59:08 +0300

    add transparent status bar

--- app/src/main/java/com/example/movie_ticket_reservation/MainActivity.kt
+++ app/src/main/java/com/example/movie_ticket_reservation/MainActivity.kt
@@ -1,14 +1,19 @@
 package com.example.movie_ticket_reservation
 
+import TransparentStatusBar
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.view.WindowCompat
+import com.example.movie_ticket_reservation.ui.screens.MovieDetailsScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
-
+            TransparentStatusBar()
+            MovieDetailsScreen()
         }
     }
 }
