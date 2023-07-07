ba75611c8e1ec3174473b28619a2d16f6d20bc72
Haidy <haidyabugom3a@gmail.com>
2023-07-07 08:18:39 +0300

    add blured images in view model

--- app/src/main/java/com/example/movie_ticket_reservation/ui/screens/movies/MoviesViewModel.kt
+++ app/src/main/java/com/example/movie_ticket_reservation/ui/screens/movies/MoviesViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.movie_ticket_reservation.ui.screens.movies
 
 import androidx.lifecycle.ViewModel
+import com.example.movie_ticket_reservation.R
 import com.example.movie_ticket_reservation.ui.ui_states.MovieUiState
 import com.example.movie_ticket_reservation.ui.ui_states.MoviesUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -20,22 +21,25 @@ class MoviesViewModel : ViewModel() {
     private fun getAllMovies() {
         val movies = listOf(
             MovieUiState(
-                name = "Fantastic Beasts: The Secrets of Dumbledore",
-                imageUrl = "https://assets.mugglenet.com/wp-content/uploads/2022/02/FMNajsRWYAskvX9.jpg",
-                nowShowing = true,
-                movieGenre = listOf("Fantasy", "Adventure"),
-                movieTime = "2h 23m"
-            ),
-            MovieUiState(
                 name = "Morbius",
                 imageUrl = "https://pbs.twimg.com/media/FFznqGlVIAEjTfM?format=jpg&name=large",
+                imageBlur = R.drawable.image_movie1_blured,
                 nowShowing = true,
                 movieGenre = listOf("Action", "Adventure", "Horror"),
                 movieTime = "1h 44m"
             ),
             MovieUiState(
+                name = "Fantastic Beasts: The Secrets of Dumbledore",
+                imageUrl = "https://assets.mugglenet.com/wp-content/uploads/2022/02/FMNajsRWYAskvX9.jpg",
+                imageBlur = R.drawable.image_movie2_blured,
+                nowShowing = true,
+                movieGenre = listOf("Fantasy", "Adventure"),
+                movieTime = "2h 23m"
+            ),
+            MovieUiState(
                 name = "Doctor Strange in the Multiverse of Madness",
                 imageUrl = "https://cdn.kinocheck.com/i/mjcd3f6x6m.jpg",
+                imageBlur = R.drawable.image_movie3_blured,
                 nowShowing = true,
                 movieGenre = listOf("Action","Adventure","Fantasy"),
                 movieTime = "2h 6m"
