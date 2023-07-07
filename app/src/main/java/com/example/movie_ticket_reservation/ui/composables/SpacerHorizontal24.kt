146d393253a67ea57c3459c5cba7c2344da9c5db
Haidy <haidyabugom3a@gmail.com>
2023-07-07 18:03:45 +0300

    add more spacers

---
+++ app/src/main/java/com/example/movie_ticket_reservation/ui/composables/SpacerHorizontal24.kt
@@ -0,0 +1,13 @@
+package com.example.movie_ticket_reservation.ui.composables
+
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SpacerHorizontal24(){
+    Spacer(modifier = Modifier.fillMaxHeight().width(24.dp))
+}
