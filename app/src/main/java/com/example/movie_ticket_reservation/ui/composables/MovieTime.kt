33d974d1b13c05533873791a094d4d80f7df879b
Haidy <haidyabugom3a@gmail.com>
2023-07-07 18:05:18 +0300

    make movie time centered vertically

--- app/src/main/java/com/example/movie_ticket_reservation/ui/composables/MovieTime.kt
+++ app/src/main/java/com/example/movie_ticket_reservation/ui/composables/MovieTime.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -13,18 +14,20 @@ import com.example.movie_ticket_reservation.ui.theme.Gray100
 import com.example.movie_ticket_reservation.ui.theme.Typography
 
 @Composable
-fun MovieTime(time:String){
-    Row {
+fun MovieTime(time: String) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
         Icon(
             painter = painterResource(id = R.drawable.icon_clock),
             contentDescription = "",
-            modifier = Modifier.padding(8.dp),
+            modifier = Modifier.padding(vertical = 8.dp).padding(start = 8.dp, end = 4.dp),
             tint = Gray100
         )
         Text(
             text = time,
             style = Typography.displaySmall,
-            modifier = Modifier.padding(top = 8.dp, end = 8.dp)
+            modifier = Modifier.padding(end = 8.dp)
         )
 
     }
